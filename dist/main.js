05ab1382b74494e6472b77ef29bbae349a5907ea
alan <alanarostegui@hotmail.com>
2021-02-22 17:36:12 -0600

    number, boolean and string and add package json

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,35 @@
 "use strict";
-console.log('este es el main');
+console.log("este es el main");
+// Number
+// Explicito
+var phone;
+phone = 1;
+phone = 38438492;
+// phone = 'hello'; // Error
+// Inferido
+var phoneNumber = 38438492;
+phoneNumber = 123;
+// phoneNumber = true: // Error por tipo de dato
+// Hexadecimales
+var hex = 0xf00d;
+var binary = 10;
+var octal = 484;
+//Tipo Boolean
+// Tipado Explicito
+var isPro;
+isPro = true;
+// isPro = 1; // Error
+// Inferido
+var isUserPro = false;
+isUserPro = true;
+// isUserPro = 10; // Error
+// Tipo String
+// Tipado Explicito
+var username = "Alan_Maranto";
+username = "Alan";
+// username = true; // Error por tipo de dato
+// Template String
+// Uso de back-tick ``
+var userInfo;
+userInfo = "\n    User Info:\n    username: " + username + "\n    firstName: " + (username + " Maranto ") + "\n    phone: " + phone + "\n    isPro: " + isPro + "\n";
+console.log("userinfo", userInfo);
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,48 @@
-console.log('este es el main')
+console.log("este es el main");
+
+// Number
+// Explicito
+let phone: number;
+phone = 1;
+phone = 38438492;
+// phone = 'hello'; // Error
+
+// Inferido
+let phoneNumber = 38438492;
+phoneNumber = 123;
+// phoneNumber = true: // Error por tipo de dato
+
+// Hexadecimales
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
+
+//Tipo Boolean
+// Tipado Explicito
+let isPro: boolean;
+isPro = true;
+// isPro = 1; // Error
+
+// Inferido
+let isUserPro = false;
+isUserPro = true;
+// isUserPro = 10; // Error
+
+// Tipo String
+// Tipado Explicito
+let username: string = "Alan_Maranto";
+username = "Alan";
+// username = true; // Error por tipo de dato
+
+// Template String
+// Uso de back-tick ``
+let userInfo: string;
+userInfo = `
+    User Info:
+    username: ${username}
+    firstName: ${username + " Maranto "}
+    phone: ${phone}
+    isPro: ${isPro}
+`;
+
+console.log("userinfo", userInfo);
